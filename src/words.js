6e15210aa4167f74917b19754196884f0843f8a2
Kenneth P. Barquero <kpalmab@icloud.com>
2021-06-15 01:08:01 -0600

    Initial commit

---
+++ src/words.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    text: "partido",
+    value: 268
+  },
+  {
+    text: "¿cuál",
+    value: 134
+  },
+  {
+    text: "villalta",
+    value: 459
+  },
+  {
+    text: "homicidios",
+    value: 168
+  },
+  {
+    text: "consumidores",
+    value: 184
+  },
+  {
+    text: "clasificar",
+    value: 128
+  },
+  {
+    text: "marcela",
+    value: 92
+  },
+  {
+    text: "chapín",
+    value: 513
+  },
+  {
+    text: "cerca",
+    value: 305
+  },
+  {
+    text: "febrero",
+    value: 301
+  },
+  {
+    text: "calidad",
+    value: 210
+  },
+  {
+    text: "parque",
+    value: 210
+  },
+  {
+    text: "blanco",
+    value: 127
+  }
+];
